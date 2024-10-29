178290b7f0a52fdd8dbe5fb911a6c04f0e9b1a85
Christopher Bartling <chris.bartling@gmail.com>
2024-10-29 18:08:19 -0500

    feat: setting up providers

--- experiments/application-insights/versions.tf
+++ experiments/application-insights/versions.tf
@@ -2,11 +2,26 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source  = "registry.terraform.io/hashicorp/azurerm"
       version = "~> 4.7.0"
     }
+    kubernetes = {
+      source  = "registry.terraform.io/hashicorp/kubernetes"
+      version = "~> 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.11.3"
+    }
+    helm = {
+      source  = "registry.terraform.io/hashicorp/helm"
+      version = "~> 2.0.2"
+    }
+    # random = {
+    #   source  = "registry.terraform.io/hashicorp/random"
+    #   version = "~> 3.0.0"
+    # }
   }
-
   required_version = ">= 1.1.0"
 }
 
