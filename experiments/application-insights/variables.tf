0573917181f2b23cbb81ef6e5a52d22c24702314
Christopher Bartling <chris.bartling@gmail.com>
2024-10-29 17:30:42 -0500

    feat: creating resource group

---
+++ experiments/application-insights/variables.tf
@@ -0,0 +1,18 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+}
+
+variable "tag_environment" {
+  default = "Demo"
+}
+
+
+variable "location" {
+  default = "West US"
+}
+
+variable "resource_group_name" {
+  default = "application-insights-rg"
+}
+
+
